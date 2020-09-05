2bc13ed8c9eb546178b83c54b28584d06126a125
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-09-05 22:16:24 +0530

    Day-1

---
+++ Claswork_batch13.java
@@ -0,0 +1,270 @@
+
+Welcome to APSSDC Android Application Development
+		Android Application Development(AAD)
+		Studnets Training Program(STP)
+		Batch 13 Timings 8.00 Am to 10.00Am
+		From 24th Aug 2020 to 12th Sept 2020
+
+Day-1
+====Instuctions====
+1.Post the doubts on the chats box
+2.mute mic and turn of the webcam 
+3.user name:Name-Roll number-College name
+session will start 8.10 am 
+
+All of you please rename your user name below format
+		Name-Roll number-College name
+------------------------------------
+Mr.Muneiah tellakula
+
+Ms.Devi Anusha 
+
+=====What is an Android ===
+Android is a Platform which Mobile Oparating System based on Linux kernl
+
+History Of Android :
+2005 --Android Inc for Developing an OS for Digital Camera 
+Key Developler of android 
+	Andy Rubin 
+	Rich Minar
+	Nic Sears
+	Crish White 
+They went see a deep losses 
+Diverte to Mobile OS ---2007 
+2007 -->Google Adapted this Android Inc 
+
+	-->2008 Google Released First Mobile OS based mobile
+	-->Htc ->Brand Name HTC-Dream 
+Google started Android OS names As Alphabits 
+Alfa
+Beta
+Cupcake 
+Donut
+Eclar
+Froyo
+Gingger bread
+Honey Comb 
+Ice Cream Sandwich
+Jelly Bean
+Kit Kat
+Lolipop
+MarshMellow 
+Nougat
+Oreo-->Google stopped OS as Alphabits
+Pie -->Android 9
+Q-->adroid 10 
+R-->Android 11
+---------Android Stack-------
+MVC-Model View Controller
+MVVM-Model View ViewModel 
+-------5 layers------
+
+1.System Apps And User Apps 
+2.JAVA API Framework  
+3.Runtime and Some Other Liberires 
+4.HAL(Harware Abstartion layer)
+5.Linux Kernal 
+
+1.System Apps And User Apps :
+	System apps:Default Applications ,no special Staus 
+		ex:messages,contacts app,clock,calculator..etc
+	UserApps :User Can Download From Externally 
+		ex:pubg,instagram,whatsup,filpcart...etc
+2.JAVA API Framework:
+	JVM-Java Virtuval Mechine 
+	Provide the Interface between OS and User 
+	API-Application Programing Interface
+3.Runtime :Runtime Runtime Permissions:Have there default Liberires
+		hadle the runtime permisstions
+
+4.HAL -Harware Abstartion layer:
+	it will provide the bridge between the  Hardware componets and Software Components 
+	ex:Camera is Hardware divice 
+		Button Software componet
+5.Linux Kernal:Manage the Resourse 
+	power 
+	storage 
+Google->Google android Developler fundamental v2 
+
+HTMl-Predefiend tags 
+	Html-<Html></Html>
+	title-
+	head-
+	body-
+XML-User Defiend Tags
+	Object: laptop
+	<laptop 
+		id:12344
+		name:lennova
+		hight:15
+		width:30
+	>
+	</laptop>
+XMLNS-naming Schema 
+	Take the refarence varible 
+	android:
+--------- Day-2 Welcome to APSSDC Android Application Development---
+A Very Good Morning TO ALl 
+Today Objective :
+----------
+Basic Builng Blocks:
+	4 types :
+		1.Resouses:Images,Styles,Strings,colors..etc
+		2.Componets:Baisc Fundamentls 
+			a)Activity
+			b)Service
+			c)Broadcast Reciver
+			d)Contant Provider 
+		3.manifest:It will be provide the Entire app information and it will handle the runtime permissions 
+			ex:Internet 
+
+		4.Build Configuration:which sdk we configured..enitre app configuration information will be availabel here
+App Components :
+			a)Activity:An Acivity is known as a single screen with User interaction or user interface Components
+			ex:every app single screen 
+			b)Service:A service is long running background task without user intraction 
+				ex:music,Alarm,Notification,Downloading tsak..etc
+			c)Broadcast Reciver:Broadcast reciveres are system Announcements..
+				ex:powerconnected,disconnceted,headset plugin..etc
+				ex2:Radio Station-release the 
+					Broadcast Channals 
+						93.5,102.498.4...etc
+						when user recives that channels
+						Anteena-Headset plugin			   
+			d)Content Provider:It is providing data/content between the two apps
+				whatsup,hike,instagram,messenger,gamil.etc
+				Data(Content):Contacts,phots,audio/video,doc ..etc
+				anusha-->whatsup
+				anushamadam-hike
+				mycollegue_anusha-messenger
+				...etc
+About Android Studio IDE
+View , ViewGroup and Android Herarchy
+
+whatsup app -playstore -->upload (Compire through the Package name)
+whatsup app -playstore -->warning message 
+
+About Android Studio IDE:
+I will divide into 4 parts:
+	1.Android studo pane:Files,edit,run,build...etc
+	2.Project Files :All the Project related Files will be available 
+	3.Layout Editor :Design the Our screens 
+	4.Logcat/Monitor :Checking the logging .erorrs information will be present here
+Today Objective:
+View ,ViewGroup and ViewH
+
+Xml-Design:
+
+<Linearlayout 
+	hight:match_parent
+	width:wrap_content
+	orientation:vertical
+	>
+<Button 
+	hight:
+
+</Linearlayout>
+
+	All ViewGroups can follow the external Closing
+		ex:	<Linearlayout 
+			....
+			...
+			>
+			</Linearlayout>
+	All views can follow the internal closing
+		ex:<Button 
+				....
+				....
+				/>
+
+Day-4
+------------
+Good Evening to all 
+All of you open the yesterday Project
+
+
+
+Day-5
+------------
+Good Evening to all 
+All of you open the Hello Toast Project
+
+LifeCycle Activity
+--------------
+6types
+1.onCreate():Default callback method or initialy app is create
+2.onStart():App is start ,no longer time 
+3.onResume():In this User can interact 
+4.onPause():this is invible method,background,it will pause the app 
+5.onStop():App can stop here
+6.onDestroy():App can be removed
+---
+
+1.onCreate():
+2.onStart():
+7.onRestrt()
+3.onResume():
+4.onPause():
+5.onStop():
+6.onStroy():			
+
+
+=====Good Morning =====
+		Day -7 
+all of you open the Android Studio and Google Slides
+
+Who are attend android Workshop need to submit this Assignments
+No-Time limit ..at least submit this end of the week in future i will give more tasks 
+
+Assignments:
+1.In Hello Toast app
+	when count button click increase the count value in he textview and Display that count value in the Toast message .
+2.Simple Calculator app 
+	Take two inputs and perform basic arthametic oparations .Result value set in the textview
+	Note:If rotate the divice store the data
+3.In the UI Components take the switch/Toggle button show the it is working or not   
+Take the screenshort 
+upload your google drive or github then send me link
+----------------------------------------------------------
+Good Morning to all 
+		Day -8
+All of you open the android studio
+----------------------
+
+
+
+
+Good Morning to all Welcome to day -9 Workshop
+All of you open the android studio
+Don't leave the meeting at end of class i will share you sheet 
+so,all of you stay up to 10.00Am
+Today objectve 
+menus
+Date Picker/Time Picker
+Alert Dilog 
+13th batch
+17KD1A0522
+-------------Very Good Morning to all-----
+					Day 11
+All of you open the Android studio
+
+----------------Day 12----------
+A very Good Morning to all 
+
+all of you open the android studio
+
+thank you guys ..for your wishes ..
+
+Recyclerview
+Components in Recyclerview:
+6 types :
+Data:set of information
+Recyclerview:Container or view class
+Layout:common layout design for every row of item
+LayoutManager:It will Control the all layouts 
+	3types of the layout LayoutManagers
+		1.LinearLayoutManager
+		2.GridLayoutManager
+		3.StraggedGrilayoutmangar
+Adapter:It provide the enviroment or platform 
+ViewHolder:it will holdview and also unhold the view
